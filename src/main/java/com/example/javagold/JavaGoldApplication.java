a1373e63c3cf700092a6dd18c3392e9675a9608a
Shinya-Takiyoshi <garo.savior@gmail.com>
2023-04-26 16:20:44 +0900

    first commit

---
+++ src/main/java/com/example/javagold/JavaGoldApplication.java
@@ -0,0 +1,13 @@
+package com.example.javagold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javagold/JavaGoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javagold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
