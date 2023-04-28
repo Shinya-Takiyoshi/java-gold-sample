357ce4e6d0cbd75133a6083d23da3898241bd225
Shinya-Takiyoshi <garo.savior@gmail.com>
2023-04-28 12:24:59 +0900

    コメント追加　2章の実装

--- src/main/java/com/example/javagold/secondChapter/FunctionSample.java
+++ src/main/java/com/example/javagold/secondChapter/FunctionSample.java
@@ -7,9 +7,11 @@ public class FunctionSample {
     Function<Integer,Integer> b = x -> x * 2;
 
     public Integer andThen(){
+        // 処理順：a -> b
        return  a.andThen(b).apply(5);
     }
     public Integer compose(){
+        // 処理順：b -> a
         return  a.compose(b).apply(5);
     }
 
